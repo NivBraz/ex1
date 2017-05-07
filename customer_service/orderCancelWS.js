106be8da1c2153fb9ebb40c8dc6f5f768e0150ef
Nivo <nivbraz90@gmail.com>
2017-05-07 15:25:13 +0300

    multiGet

--- customer_service/orderCancelWS.js
+++ customer_service/orderCancelWS.js
@@ -32,18 +32,28 @@ class Client extends events{
 
     }
     getStatusLangMovie(status,lang){
+        var str = `{"ordered movies" : [`;
+        var counter =0;
         let found = false;
         for(let i in data.orderMovies){
             var orderMovie = data.orderMovies[i];
             if((orderMovie.status == status)&&(orderMovie.language == lang)){
                 console.log(`movie found: ${orderMovie.name}`);
                 found = true;
-                return {"ordered movie":orderMovie};
+                str += `{"movie name" :  "${orderMovie.name}",
+                         "movie id" : "${orderMovie.id}"
+                },`;
+                counter ++;
             }
         }
         if(!found){
             console.log("movie not found");
             return {"error" : "movie not found"};
+        }
+        else{
+            str += `{"number of movies" : "${counter}"}]}`;
+            var obj = JSON.parse(str);
+            return obj;
         }
     }
 };
